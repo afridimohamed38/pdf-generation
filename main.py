1ad72dbc751310cffb268deed5d4920cbe4e4ddf
Afridi <afridimohamed38@gmail.com>
2023-10-25 15:47:47 +0530

    D24 : Creating headers and lines

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
-pdf.set_font(family="Times",size=12)
-pdf.cell(w=0, h=12, txt="Hello There", align="L", ln=1, border=1)
-pdf.cell(w=0, h=12, txt="Hello There", align="L", ln=1, border=1)
+df = pd.read_csv("topics.csv", sep=",")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)  # gray color
+    pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
+    pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
