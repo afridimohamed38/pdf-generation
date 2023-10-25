760fb39756e9873e5474b34480e2c6c5113288a9
Afridi <afridimohamed38@gmail.com>
2023-10-25 18:16:08 +0530

    D24 : Adding 10mm distance

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="B", size=24)
         pdf.set_text_color(100, 100, 100)  # gray color
         pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
-        pdf.line(10, 21, 200, 21)
+
+        # Add multiple lines with height 10mm distance
+        pdf.set_text_color(0, 0, 100)  # blue color
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
         # line break
         pdf.ln(265)
