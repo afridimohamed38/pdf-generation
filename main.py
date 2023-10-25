1c61911e9213f08e077c1a4dd04bc5bcabaac45b
Afridi <afridimohamed38@gmail.com>
2023-10-25 17:03:19 +0530

    D24 : Adding multiple pages

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv", sep=",")
 
 for index, row in df.iterrows():
-    pdf.add_page()
-    pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100, 100, 100)  # gray color
-    pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+    for i in range(row['Pages']):
+        pdf.add_page()
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.set_text_color(100, 100, 100)  # gray color
+        pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
+        pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
